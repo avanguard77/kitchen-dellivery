432a5dbbfddd2b41f29987adf5360e71c4029f83
avangaurd2 <Avanguard1383.w@gmail.com>
2025-02-26 17:23:22 +0330

    b01

--- NewMonoBehaviourScript.cs
+++ NewMonoBehaviourScript.cs
@@ -5,7 +5,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        transform.position+=new Vector3(0f,0f,0f);
     }
 
     // Update is called once per frame
