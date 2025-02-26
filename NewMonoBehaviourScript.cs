2546436e7f4ea6f9c2ed3c07eb353703a8050705
avangaurd2 <Avanguard1383.w@gmail.com>
2025-02-26 21:08:14 +0330

    b03

--- NewMonoBehaviourScript.cs
+++ NewMonoBehaviourScript.cs
@@ -2,13 +2,11 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   
     void Start()
     {
         transform.position+=new Vector3(0f,0f,0f);
     }
-
-    // Update is called once per frame
     void Update()
     {
         
