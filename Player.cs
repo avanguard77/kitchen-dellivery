8022d112344b60ce941814dcea3a9476a208769b
avangaurd2 <Avanguard1383.w@gmail.com>
2025-02-27 17:14:07 +0330

    b06

--- Player.cs
+++ Player.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float rotateSpead = 5f;
+    private void Update()
     {
-        
-    }
+        Vector3 inputvector = new Vector2(0f, 0f);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputvector.y += 1f;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            inputvector.y -= 1f;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            inputvector.x -= 1f;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            inputvector.x += 1f;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        inputvector = inputvector.normalized;
+
+        Vector3 movementvector = new Vector3(inputvector.x * moveSpeed, 0f, inputvector.y * moveSpeed);
+        transform.position += movementvector * Time.deltaTime * moveSpeed;
         
+        // transform.LookAt(transform.position + movementvector, Vector3.up);
+        transform.forward =Vector3.Slerp(movementvector, Vector3.up, Time.deltaTime*rotateSpead); 
     }
 }
